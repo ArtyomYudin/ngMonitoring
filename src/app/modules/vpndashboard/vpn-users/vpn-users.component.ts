63438e460eb6096a2d23ab1f719e08cc8a86a980
Artyom Yudin <ntf@inbox.ru>
2021-02-04 15:37:31 +0300

    vpn dashboard ip info

--- src/app/modules/vpndashboard/vpn-users/vpn-users.component.ts
+++ src/app/modules/vpndashboard/vpn-users/vpn-users.component.ts
@@ -4,10 +4,13 @@ import { Observable } from 'rxjs';
 import { FormGroup, FormControl } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 
+import { HttpClient } from '@angular/common/http';
+
 import { WebsocketService } from '@app/services/websocket.service';
 
 import { VPNUserModel } from '@models/vpn-user.model';
 import { VPNSessionModel } from '@models/vpn-session.model';
+
 import { Event } from '@services/websocket.service.event';
 
 import { AccountFilter } from '@modules/vpndashboard/vpndashboard.filter.class';
@@ -28,9 +31,13 @@ export class VpnUsersComponent implements OnInit {
 
   public ipAddress: string;
 
+  public ipInfo;
+
+  public gridIpInfo;
+
   sessionDatePick: FormGroup;
 
-  constructor(private datePipe: DatePipe, private wsService: WebsocketService) {}
+  constructor(private datePipe: DatePipe, private http: HttpClient, private wsService: WebsocketService) {}
 
   public ngOnInit(): void {
     this.eventVPNAllUsersArray$ = this.wsService.on<VPNUserModel>(Event.EV_VPN_ALL_USERS);
@@ -48,11 +55,33 @@ export class VpnUsersComponent implements OnInit {
       this.wsService.send('get-vpn-user-info', { sessionDate: this.datePipe.transform(new Date(), 'yyyy-MM-dd'), vpnAccount: account });
       this.eventVPNUserStatusArray$.subscribe(data => {
         this.ipAddress = data;
+        this.http.get(`http://ip-api.com/json/${this.ipAddress}?lang=ru&fields=status,country,regionName,city,isp,org,as`).subscribe(
+          info => {
+            this.ipInfo = info;
+            // console.log(this.ipInfo);
+          },
+          error => {
+            this.ipInfo = null;
+          },
+        );
       });
     }
   }
 
   public setVPNSessionDate(event: Date, account: string): void {
     this.wsService.send('get-vpn-user-session', { sessionDate: this.datePipe.transform(event, 'yyyy-MM-dd'), vpnAccount: account });
+  }
+
+  public getIpInfo(ip: string): void {
+    if (ip !== this.gridIpInfo?.query)
+      this.http.get(`http://ip-api.com/json/${ip}?lang=ru&fields=status,country,regionName,city,isp,org,as,query`).subscribe(
+        info => {
+          this.gridIpInfo = info;
+          // console.log(this.ipInfo);
+        },
+        error => {
+          this.gridIpInfo = null;
+        },
+      );
   }
 }
