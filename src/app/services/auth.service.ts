7723cc258cfdb5184ddc55c65f19eefec07cb161
Artyom Yudin <yudin.artyom@gmail.com>
2021-08-20 10:43:22 +0300

    bug fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, BehaviorSubject} from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthUser } from '@models/authuser.model';
@@ -10,10 +10,10 @@ import { AuthUser } from '@models/authuser.model';
 })
 export class AuthenticationService {
   public currentUser$: Observable<AuthUser>;
+
   private currentUserSubject$: BehaviorSubject<AuthUser>;
 
-  constructor(private http: HttpClient,
-              private jwtHelper: JwtHelperService) {
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {
     this.currentUserSubject$ = new BehaviorSubject<AuthUser>(JSON.parse(localStorage.getItem('ngMonitoring')));
     this.currentUser$ = this.currentUserSubject$.asObservable();
   }
@@ -29,28 +29,29 @@ export class AuthenticationService {
       }),
     };
 
-    return this.http.post<AuthUser>('http://172.20.4.195:3000/api/auth', { email, password }, httpOptions)
-            .pipe(map(authUser => {
-              if (authUser && authUser.token) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('ngMonitoring', JSON.stringify(authUser));
-                this.currentUserSubject$.next(authUser);
-              }
-              return authUser;
-            }));
+    return this.http.post<AuthUser>('http://172.20.4.195:3000/api/auth', { email, password }, httpOptions).pipe(
+      map(authUser => {
+        if (authUser && authUser.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('ngMonitoring', JSON.stringify(authUser));
+          this.currentUserSubject$.next(authUser);
+        }
+        return authUser;
+      }),
+    );
   }
 
   public logout() {
-        // remove user from local storage to log user out
+    // remove user from local storage to log user out
     localStorage.removeItem('ngMonitoring');
     this.currentUserSubject$.next(null);
   }
 
   public isAuthenticated(): boolean {
     if (localStorage.getItem('ngMonitoring')) {
-    //  const token = JSON.parse(localStorage.getItem('ngMonitoring')).token;
-    // console.log(this.jwtHelper);
-    return !this.jwtHelper.isTokenExpired();
+      //  const token = JSON.parse(localStorage.getItem('ngMonitoring')).token;
+      // console.log(this.jwtHelper);
+      return !this.jwtHelper.isTokenExpired();
     }
     return false;
   }
