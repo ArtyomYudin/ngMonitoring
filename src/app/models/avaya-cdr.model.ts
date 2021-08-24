b8e34d04f38d7a130ff56eaf697a6d582677c5e5
Artyom Yudin <yudin.artyom@gmail.com>
2021-08-24 17:24:42 +0300

    fix avaya cdr

--- src/app/models/avaya-cdr.model.ts
+++ src/app/models/avaya-cdr.model.ts
@@ -2,8 +2,8 @@ export class AvayaCDRModel {
   public callDateTime: string;
   public callDuration: number;
   public callingNumber: string;
-  public callingName: string;
   public calledNumber: string;
-  public calledName: string;
   public callCode: string;
+  public callingName: string;
+  public calledName: string;
 }
