0fcffc0bfb83b9f2fa9d3ea478f009cd12f68612
Artyom Yudin <yudin.artyom@gmail.com>
2021-08-26 17:44:40 +0300

    fix avaya cdr

--- src/app/pipe/avayadurationconvert.pipe.ts
+++ src/app/pipe/avayadurationconvert.pipe.ts
@@ -5,10 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AvayaDurationConvertPipe implements PipeTransform {
   public transform(value: number): string {
-    if (value < 60) {
-      return value + ' сек.';
-    } else {
-      return Math.floor(value / 60) + ' мин. ' + (value % 60) + ' сек.';
-    }
+    const h = Math.floor(value / 3600);
+    const m = Math.floor((value % 3600) / 60);
+    const s = Math.floor((value % 3600) % 60);
+
+    // var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
+    // var mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : '';
+    // var sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : '';
+    const hDisplay = h > 0 ? h + ' ч. ' : '';
+    const mDisplay = m > 0 ? m + ' мин. ' : '';
+    const sDisplay = s > 0 ? s + ' сек.' : '';
+    return hDisplay + mDisplay + sDisplay;
   }
 }
