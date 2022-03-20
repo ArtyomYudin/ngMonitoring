f0cadd2c1ef8a497672bff95364f64acef9340f2
Artyom Yudin <yudin.artyom@gmail.com>
2022-03-20 12:48:20 +0300

    fix reuse route startegy

--- src/app/custom-reuse-strategy.ts
+++ src/app/custom-reuse-strategy.ts
@@ -4,31 +4,27 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CustomReuseStrategy implements RouteReuseStrategy {
-  private routesToCache: string[] = ['cached_skud', 'cached_monitoring', 'cached_vpn', 'cached_dhcp', 'cached_avaya'];
+  private handlers: { [key: string]: DetachedRouteHandle } = {};
 
-  private storedRouteHandles = new Map<string, DetachedRouteHandle>();
+  constructor() {}
 
-  // Decides if the route should be stored
-  public shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    return this.routesToCache.indexOf(route.data.key) > -1;
+  public shouldDetach(_route: ActivatedRouteSnapshot): boolean {
+    return true;
   }
 
-  // Store the information for the route we're destructing
   public store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-    this.storedRouteHandles.set(route.data.key, handle);
+    this.handlers[route.data.key] = handle;
   }
 
-  // Return true if we have a stored route object for the next route
   public shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    return this.storedRouteHandles.has(route.data.key);
+    return !!this.handlers[route.data.key] && !!route.component;
   }
 
-  // If we returned true in shouldAttach(), now return the actual route data for restoration
-  public retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-    return this.storedRouteHandles.get(route.data.key);
+  public retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
+    if (!route.component) return null;
+    return this.handlers[route.data.key];
   }
 
-  // Reuse the route if we're going to and from the same route
   public shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
     return future.routeConfig === curr.routeConfig;
   }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,7 +1,7 @@
 /***************************************************************************************************
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
-import '@angular/localize/init';
+// import '@angular/localize/init';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -22,7 +22,7 @@ import '@angular/localize/init';
  * BROWSER POLYFILLS
  */
 
- /** IE9, IE10 and IE11 requires all of the following polyfills. */
+/** IE9, IE10 and IE11 requires all of the following polyfills. */
 // import 'core-js/es6/symbol';
 // import 'core-js/es6/object';
 // import 'core-js/es/function';
@@ -75,8 +75,8 @@ import '@angular/localize/init';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+// import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js';
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
